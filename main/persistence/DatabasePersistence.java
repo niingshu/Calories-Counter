2868292d5e0e1abfec67bc65d92057c02e341423
nshu15 <nshu15@students.ubc.ca>
2026-01-05 23:32:49 -0800

    import postgresql

---
+++ main/persistence/DatabasePersistence.java
@@ -0,0 +1,12 @@
+package persistence;
+/*
+this file is resposible for 
+opening database connections, executing sql, converting rows <-> java objects
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager; 
+
+public class DatabasePersistence {
+    
+}
