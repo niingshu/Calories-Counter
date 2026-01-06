7121419aadedf600a5c89d57e45c799d36f23e24
nshu15 <nshu15@students.ubc.ca>
2026-01-06 09:28:35 -0800

    test connection between postgresql and java

---
+++ test/persistence/DatabasePersistenceTest.java
@@ -0,0 +1,28 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//these are java's build-in JDBC API (dont talk to postgres themselves, .jar does that)
+
+public class DatabasePersistenceTest {
+    public static void main(String[] args) {
+        //database connection details: 
+        String url = "jdbc:postgresql://localhost:5432/calories_counter"; 
+        //use postgresql, connected to my computer, on port 5432, to database: calories counter
+        String user = "ningshu";
+        String password = "password";
+        //depends on the setup password
+
+        //try to connect: 
+        try (Connection con = DriverManager.getConnection(url, user, password)) { //test
+            //connection con: opens the connection and closes it safely when done 
+            //if reach herel, then connection works 
+            System.err.println("Connected to database successfully");
+        } catch (SQLException e) {
+            //if something goes wrong, catch it here and print 
+            System.err.println("Connection failed.");
+            e.printStackTrace();
+        }
+    }
+}
